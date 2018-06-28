42c2a58b13a3a505f6a3fdd086a4c1005f8c91ae
Nouman Arshad <noumanarshad0320@gmail.com>
2018-06-28 05:59:38 +0500

    Added JWT Authentication

--- Checkar_webAPI_core/Checkar_webAPI_core/Controllers/User_loginController.cs
+++ Checkar_webAPI_core/Checkar_webAPI_core/Controllers/User_loginController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Cors;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 namespace Checkar_webAPI_core.Controllers
 {
@@ -13,6 +17,7 @@ namespace Checkar_webAPI_core.Controllers
     [EnableCors("AllowAnyOrigin")]
     public class User_loginController : Controller
     {
+
         
         // GET api/values
         [HttpGet]
@@ -31,9 +36,9 @@ namespace Checkar_webAPI_core.Controllers
 
         // POST api/values
         [HttpPost]
-        public Boolean Post([FromBody]Classes.User user)
+        public IActionResult Post([FromBody]Classes.User user)
         {
-            Boolean LoginCheck = false;
+            
             try
             {
                 if (user != null)
@@ -48,6 +53,24 @@ namespace Checkar_webAPI_core.Controllers
                     {
                         // To be executed whe login is successful
 
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, UserLogin.Email),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
+
+                        string privateSecretKey = "OfED+KgbZxtu4e4+JSQWdtSgTnuNixKy1nMVAEww8QL3IN3idcNgbNDSSaV4491Fo3sq2aGSCtYvekzs7JwXJnNAyvDSJjfK/7M8MpxSMnm1vMscBXyiYFXhGC4wqWlYBE828/5DNyw3QZW5EjD7hvDrY5OlYd4smCTa53helNnJz5NT9HQaDbE2sMwIDAQABAoIBAEs63TvT94njrPDP3A/sfCEXg1F2y0D/PjzUhM1aJGcRiOUXnGlYdViGhLnnJoNZTZm9qI1LT0NWcDA5NmBN6gcrk2EApyTt1D1i4AQ66rYoTF9iEC4Wye28v245BYESA6IIelgIxXGsVyllERsbTkaphzibbYfHmvwMxkn135Zfzd/NOXl/O32vYIomzrNEP+tN2WXhhG8c8+iZ8PErBV3CqrYogYy97d2CeQbXcpd5unPiU4TK0nnzeBAXdgeYuJHFC45YHl9UvShRoe6CHR47ceIGp6WMc5BTyyTkZpctuYJTwaChdj/QuRSkTYmn6jFL+MRfYQJ8VVwSVo5DbkECgYEA4/YIMKcwObYcSuHzgkMwH645CRDoy9M98eptAoNLdJBHYz23U5IbGL1+qHDDCPXxKs9ZG7EEqyWezq42eoFoebLA5O6/xrYXoaeIb094dbCF4D932hAkgAaAZkZVsSiWDCjYSV+JoWX4NVBcIL9yyHRhaaPVULTRbPsZQWq9+hMCgYEA48j4RGO7CaVpgUVobYasJnkGSdhkSCd1VwgvHH3vtuk7/JGUBRaZc0WZGcXkAJXnLh7QnDHOzWASdaxVgnuviaDi4CIkmTCfRqPesgDR2Iu35iQsH7P2/o1pzhpXQS/Ct6J7/GwJTqcXCvp4tfZDbFxS8oewzp4RstILj+pDyWECgYByQAbOy5xB8GGxrhjrOl1OI3V2c8EZFqA/NKy5y6/vlbgRpwbQnbNy7NYj+Y/mV80tFYqldEzQsiQrlei78Uu5YruGgZogL3ccj+izUPMgmP4f6+9XnSuN9rQ3jhy4k4zQP1BXRcim2YJSxhnGV+1hReLknTX2IwmrQxXfUW4xfQKBgAHZW8qSVK5bXWPjQFnDQhp92QM4cnfzegxe0KMWkp+VfRsrw1vXNx";
+                        var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(privateSecretKey));
+
+                    
+                    var token = new JwtSecurityToken(
+                                issuer: "http://www.checkarr.com",
+                                audience: "http://www.checkarr.com",
+                                expires: DateTime.UtcNow.AddMinutes(5),
+                                claims: claims,
+                                signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+                            );
+
                         /*
                         System.Diagnostics.Debug.Print("===========================\n");
                         System.Diagnostics.Debug.Print("Login POST successful");
@@ -57,26 +80,32 @@ namespace Checkar_webAPI_core.Controllers
                         System.Diagnostics.Debug.Print("===========================\n");
                         */
 
-                        LoginCheck = true;
+                        return Ok(new
+                        {
+                            Token = new JwtSecurityTokenHandler().WriteToken(token),
+                            Type = "Bearer",
+                            Generation = DateTime.UtcNow,
+                            Expiration = token.ValidTo,
+                            Issuer = token.Issuer
+                        });
                     }
                     else
                     {
                         // To be executed the login fails
-                        LoginCheck = false;
+                        return Unauthorized();
                     }
                 }
                 else
                 {
 
-                    LoginCheck = false;
+                    return Unauthorized();
                 }
             }
             catch (Exception e)
             {
                 System.Diagnostics.Debug.Print(e.ToString());
             }
-
-            return LoginCheck;
+            return Unauthorized();
         }
 
         /*
--- Checkar_webAPI_core/Checkar_webAPI_core/Startup.cs
+++ Checkar_webAPI_core/Checkar_webAPI_core/Startup.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
+using Microsoft.IdentityModel.Tokens;
 
 namespace Checkar_webAPI_core
 {
@@ -37,7 +39,30 @@ namespace Checkar_webAPI_core
 
             }); // So that cross orgin domains can access this API
 
-           
+            String companyURL = "http://www.checkarr.com";
+            String privateSecretKey = "OfED+KgbZxtu4e4+JSQWdtSgTnuNixKy1nMVAEww8QL3IN3idcNgbNDSSaV4491Fo3sq2aGSCtYvekzs7JwXJnNAyvDSJjfK/7M8MpxSMnm1vMscBXyiYFXhGC4wqWlYBE828/5DNyw3QZW5EjD7hvDrY5OlYd4smCTa53helNnJz5NT9HQaDbE2sMwIDAQABAoIBAEs63TvT94njrPDP3A/sfCEXg1F2y0D/PjzUhM1aJGcRiOUXnGlYdViGhLnnJoNZTZm9qI1LT0NWcDA5NmBN6gcrk2EApyTt1D1i4AQ66rYoTF9iEC4Wye28v245BYESA6IIelgIxXGsVyllERsbTkaphzibbYfHmvwMxkn135Zfzd/NOXl/O32vYIomzrNEP+tN2WXhhG8c8+iZ8PErBV3CqrYogYy97d2CeQbXcpd5unPiU4TK0nnzeBAXdgeYuJHFC45YHl9UvShRoe6CHR47ceIGp6WMc5BTyyTkZpctuYJTwaChdj/QuRSkTYmn6jFL+MRfYQJ8VVwSVo5DbkECgYEA4/YIMKcwObYcSuHzgkMwH645CRDoy9M98eptAoNLdJBHYz23U5IbGL1+qHDDCPXxKs9ZG7EEqyWezq42eoFoebLA5O6/xrYXoaeIb094dbCF4D932hAkgAaAZkZVsSiWDCjYSV+JoWX4NVBcIL9yyHRhaaPVULTRbPsZQWq9+hMCgYEA48j4RGO7CaVpgUVobYasJnkGSdhkSCd1VwgvHH3vtuk7/JGUBRaZc0WZGcXkAJXnLh7QnDHOzWASdaxVgnuviaDi4CIkmTCfRqPesgDR2Iu35iQsH7P2/o1pzhpXQS/Ct6J7/GwJTqcXCvp4tfZDbFxS8oewzp4RstILj+pDyWECgYByQAbOy5xB8GGxrhjrOl1OI3V2c8EZFqA/NKy5y6/vlbgRpwbQnbNy7NYj+Y/mV80tFYqldEzQsiQrlei78Uu5YruGgZogL3ccj+izUPMgmP4f6+9XnSuN9rQ3jhy4k4zQP1BXRcim2YJSxhnGV+1hReLknTX2IwmrQxXfUW4xfQKBgAHZW8qSVK5bXWPjQFnDQhp92QM4cnfzegxe0KMWkp+VfRsrw1vXNx";
+
+            services.AddAuthentication(Options =>
+            {
+                Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                Options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).
+            AddJwtBearer(Options =>
+            {
+                Options.SaveToken = true;
+                Options.RequireHttpsMetadata = false;
+                Options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = companyURL,
+                    ValidIssuer = companyURL,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(privateSecretKey))
+
+
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +72,8 @@ namespace Checkar_webAPI_core
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
             app.UseCors("AllowAnyOrigin");
             app.UseMvc();
         }
