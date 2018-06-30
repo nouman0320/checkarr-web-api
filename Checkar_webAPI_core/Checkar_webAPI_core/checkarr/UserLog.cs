eed75c2a27aafaa1e0662a9559bf2a6ea8ec19de
Nouman Arshad <noumanarshad0320@gmail.com>
2018-06-30 20:45:46 +0500

    Updated model

--- Checkar_webAPI_core/Checkar_webAPI_core/checkarr/UserLog.cs
+++ Checkar_webAPI_core/Checkar_webAPI_core/checkarr/UserLog.cs
@@ -5,9 +5,11 @@ namespace Checkar_webAPI_core.checkarr
 {
     public partial class UserLog
     {
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public string Gender { get; set; }
-        public string Password { get; set; }
+        public int IduserLog { get; set; }
+        public string UserFullname { get; set; }
+        public string UserEmaill { get; set; }
+        public string UserSex { get; set; }
+        public DateTimeOffset UserTimestamp { get; set; }
+        public string UserPassword { get; set; }
     }
 }
