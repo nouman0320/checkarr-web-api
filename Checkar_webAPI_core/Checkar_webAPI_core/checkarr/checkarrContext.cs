addd6bf7c50b827692a62eb3d93be380e455bdeb
msk171 <mshahnawaz.nuces@gmail.com>
2018-07-01 22:21:27 +0500

    Commenting

--- Checkar_webAPI_core/Checkar_webAPI_core/checkarr/checkarrContext.cs
+++ Checkar_webAPI_core/Checkar_webAPI_core/checkarr/checkarrContext.cs
@@ -13,7 +13,7 @@ namespace Checkar_webAPI_core.checkarr
             if (!optionsBuilder.IsConfigured)
             {
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=Password420;database=checkarr");
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=12government$;database=checkarr");
             }
         }
 
