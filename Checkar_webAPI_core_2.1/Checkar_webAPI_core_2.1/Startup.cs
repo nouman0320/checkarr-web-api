ff5e451fb4f01643444653e586dea481481cf8b0
Nouman Arshad <noumanarshad0320@gmail.com>
2018-12-26 00:14:29 +0500

    changed ConnectionString

--- Checkar_webAPI_core_2.1/Checkar_webAPI_core_2.1/Startup.cs
+++ Checkar_webAPI_core_2.1/Checkar_webAPI_core_2.1/Startup.cs
@@ -36,7 +36,7 @@ namespace Checkar_webAPI_core._1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<checkarrContext>(x => x.UseMySql(Configuration.GetConnectionString("AzureConnection")));
+            services.AddDbContext<checkarrContext>(x => x.UseMySql(Configuration.GetConnectionString("NoumanConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors(options =>
             {
